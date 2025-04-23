81318355784327e2d8c6fec3207453b5e735c590
Thiag0h <thiagoth3005@gmail.com>
2025-04-22 21:29:49 -0300

    v1.8

--- Javascript/novidades.js
+++ Javascript/novidades.js
@@ -21,32 +21,24 @@ const buttons = document.querySelectorAll(".filter-btn");
 
     // Adiciona evento de clique para o botão "Ver mais"
 
-    const txt =
-    document.querySelector(".text");
-    const textInitialHeight = txt.clientHeight;
-    const overflow = document.querySelector(".overflow");
-    const btn = document.querySelector(".toggle");
-
-    btn.addEventListener("click", initToggle);
-
-    function initToggle(e) {
-        const isMore = e.target.dataset.state === "more";
-
-    txt.style.maxHeight = isMore
-        ? `${txt.scrollHeight}px`
-        : `${textInitialHeight}px`;
-
-        e.target.setAttribute(
-            "data-state",
-            e.target.dataset.state === "more" ? "less" : "more"
-        );
-
-        e.target.innerHTML =
-        e.target.dataset.state === "more" ? "Veja mais " : "Veja menos ";
-
-        overflow.setAttribute(
-            "data-state",
-            e.target.dataset.state === "more" ? "visible" : "hidden"
-        );
-    }
+    document.querySelectorAll('.toggle').forEach((btn) => {
+      const txt = btn.closest('.description').querySelector('.text');
+      const overflow = txt.querySelector('.overflow');
+      const textInitialHeight = txt.clientHeight;
+  
+      btn.addEventListener('click', function () {
+          const isMore = btn.dataset.state === 'more';
+  
+          txt.style.maxHeight = isMore
+              ? `${txt.scrollHeight}px`
+              : `${textInitialHeight}px`;
+  
+          btn.setAttribute('data-state', isMore ? 'less' : 'more');
+          btn.innerHTML = isMore
+              ? 'Veja menos <i class="fa-solid fa-chevron-right arrow"></i>'
+              : 'Veja mais <i class="fa-solid fa-chevron-right arrow"></i>';
+  
+          overflow.setAttribute('data-state', isMore ? 'hidden' : 'visible');
+      });
+  });
 
